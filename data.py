ebdb2a103fd3a9bc67c87d62e97e0c6412a1f773
FilipJQ77 <filipprzygonski99@gmail.com>
2020-04-14 00:28:07 +0200

    szkielet pliku data, zrobione kodowanie i dekodowanie bloków bitów za pomocą bitu parzystości

--- data.py
+++ data.py
@@ -1,8 +1,14 @@
 import random
+import numpy as np
+
+generator_matrix = np.array(
+    ((1, 1, 0, 1), (1, 0, 1, 1), (1, 0, 0, 0), (0, 1, 1, 1), (0, 1, 0, 0), (0, 0, 1, 0), (0, 0, 0, 1)))
+
+parity_check_matrix = np.array(((1, 0, 1, 0, 1, 0, 1), (0, 1, 1, 0, 0, 1, 1), (0, 0, 0, 1, 1, 1, 1)))
 
 
-# generates random bits of data (true or false)
-def generate_random_data(amount: int) -> list:
+# generates random bits of data (true or false) todo: albo t/f albo 0/1, co lepsze?
+def generate_random_data_bool(amount: int) -> list:
     bits = []
     for i in range(0, amount):
         random_number = random.randint(0, 1)
@@ -13,9 +19,9 @@ def generate_random_data(amount: int) -> list:
     return bits
 
 
-# prints data generated with generate_random_data
-def print_data(bits: list):
-    print(f"Data of list with id:{id(bits)}: [", end='')
+# prints data generated with generate_random_data_bool
+def print_data_bool(bits: list):
+    print(f"Data of a list with id:{id(bits)}: [", end='')
     for boolean in bits:
         if boolean:
             print(1, end='')
@@ -23,4 +29,78 @@ def print_data(bits: list):
         else:
             print(0, end='')
             pass
+    print("]")
+
+
+# todo: na razie będzie robione na liczbach, potem można to najwyżej zoptymalizować
+def generate_random_data(amount: int) -> list:
+    bits = []
+    for i in range(0, amount):
+        bits.append(random.randint(0, 1))
+    return bits
+
+
+# todo zaburzanie sygnału
+def distort_bits(bits: list) -> list:
+    pass
+
+
+# todo: przekazujemy ciąg bitów, a funkcja podzieli je na 4 bitowe fragmenty, zwraca listę list(?)
+def separate_data(bits: list) -> list:
+    pass
+
+
+# encodes given list of 4 bits into a Hamming (7,4) code todo
+def encode_hamming(bits: list) -> list:
+    datamat = np.array(bits)
+    data_matrix = np.array([(bits[0]), (bits[1]), (bits[2]), (bits[3])])
+    codeword = np.multiply(generator_matrix, data_matrix)
+    print(codeword)
+    # bits = list(codeword)
+    # return bits
+
+
+# decodes given list of 7 bits of Hamming (7,4) code into 4 bits of data if possible, otherwise returns None todo
+def decode_hamming(bits: list) -> list:
+    return None
+
+
+# expects 4 bits
+def encode_parity(bits: list) -> list:
+    i = 0
+    for bit in bits:
+        i += bit
+    bits.append(i % 2)
+    return bits
+
+
+# expects 5 bits
+def decode_parity(bits: list) -> list:
+    i = 0
+    for bit in bits:
+        i += bit
+    i -= bits[-1]
+    i = i % 2
+    if i == bits[-1]:
+        bits.pop()
+        return bits
+    else:
+        return None
+
+
+# expects 4 bits, todo powtarzanie bitu razy 3, czy może 4?
+def encode_repeat(bits: list) -> list:
+    pass
+
+
+# expects
+def decode_repeat(bits: list) -> list:
+    pass
+
+
+# prints data generated with generate_random_data_bool
+def print_data(bits: list):
+    print(f"Data of a list with id:{id(bits)}: [", end='')
+    for bit in bits:
+        print(bit, end='')
     print("]")
--- main.py
+++ main.py
@@ -3,13 +3,26 @@
 # import time
 import data
 
-lista = data.generate_random_data(10)
-data.print_data(lista)
+# lista = data.generate_random_data(10)
+# data.print_data_bool(lista)
 
-lista2 = data.generate_random_data(5)
-data.print_data(lista2)
+# lista2 = data.generate_random_data(5)
+# data.print_data_bool(lista2)
 
-lista.append(True)
+# lista.append(True)
+# data.print_data_bool(lista)
+# lista2.remove(True)
+# data.print_data_bool(lista2)
+
+# lista = data.generate_random_data(10)
+# data.encode_hamming(lista)
+
+lista = data.generate_random_data(4)
 data.print_data(lista)
-lista2.remove(True)
-data.print_data(lista2)
+lista = data.encode_parity(lista)
+data.print_data(lista)
+lista = data.decode_parity(lista)
+if lista is not None:
+    data.print_data(lista)
+else:
+    print("oof")
