dc6897087c72a9d673e844d9bb65ff79bc1004ef
FilipJQ77 <filipprzygonski99@gmail.com>
2020-04-20 23:54:10 +0200

    szybka metoda w mainie do testowania kodu Hamminga

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-# używanie innych plików - po prostu import nazwa_pliku
-# import random
+import random
 # import time
 import data
 import hamming
 
 # for i in range(4, 18):
-lista1 = [1, 1, 0, 1]  # data.generate_random_data(i)
+lista1 = data.generate_random_data(11)
 data.print_data(lista1)
 lista1 = hamming.encode_hamming(lista1)
 data.print_data(lista1)
-lista2 = [0, 0, 1, 0, 1, 0, 1, 1] # [1, 0, 1, 0, 1, 0, 1, 0] # correct
-lista1 = hamming.decode_hamming(lista2)
+what_to_test = "F"  # todo albo "R", w zależności co testujesz
+lista_size = len(lista1) - 1
+if what_to_test == "F":
+    i1 = random.randint(0, lista_size)
+    lista1[i1] = data.distort_bit(lista1[i1], 0)
+else:
+    i1 = random.randint(0, lista_size)
+    lista1[i1] = data.distort_bit(lista1[i1], 0)
+    i2 = random.randint(0, lista_size)
+    while i1 == i2:
+        i2 = random.randint(0, lista_size)
+    lista1[i2] = data.distort_bit(lista1[i2], 0)
+data.print_data(lista1)
+lista1 = hamming.decode_hamming(lista1)
 data.print_data(lista1)
